3b8b10e82a95fdf13b5c92d3acf43b93b8ca0708
Jostein <josteinjhauge@me.com>
2020-10-16 15:11:30 +0200

    fjernet bilder

--- assets/Images.js
+++ assets/Images.js
@@ -10,7 +10,7 @@ export default Images = {
     // ObstacleTop should have size: 205x95
     pipeTop: require('./images/pipe_top.png'),
     // Character image size: 120x99 
-    character1: require('./images/bird1.png'), // set inn character image 1
-    character2: require('./images/bird2.png'), // set inn character image 2
-    character3: require('./images/bird3.png'), // set inn character image 3
+    character1: require('./images/spongebob.jpg'), // set inn character image 1
+    character2: require('./images/spongebob.jpg'), // set inn character image 2
+    character3: require('./images/spongebob.jpg'), // set inn character image 3
 }
